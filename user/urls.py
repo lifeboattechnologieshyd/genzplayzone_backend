b259ec5a5c5b4f5cc976be110a452a2f5cf3ceab
charanlakkam331 <charanlakkam0309@gmail.com>
2026-06-23 17:45:21 +0530

    file upload api added

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import SendOtp, VerifyOTP
+from .views import SendOtp, VerifyOTP, FileUploadView
 
 urlpatterns = [
     path("send-otp", SendOtp.as_view()),
     path("verify-otp", VerifyOTP.as_view()),
+
     # need to add apis here.
+    path("file/upload",FileUploadView.as_view()),
+
 ]
--- user/views.py
+++ user/views.py
@@ -1,9 +1,17 @@
 import random
 from datetime import timedelta
+from django.conf import settings
+
+from django.core.files.base import ContentFile
+from django.core.files.storage import default_storage
 from django.utils import timezone
+from rest_framework import status
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 from db.models.user import OTPs, UserMaster
+from shared.clients.s3 import add_unique_suffix_to_filename, sanitize_filename
 from shared.clients.sms import send_otp_sms
 from shared.utils import CustomResponse, getReferralCode
 
@@ -114,3 +122,35 @@ class VerifyOTP(APIView):
         )
 
 
+class FileUploadView(APIView):
+    permission_classes = [AllowAny]
+    parser_classes = [MultiPartParser, FormParser]
+
+    def post(self, request, *args, **kwargs):
+        files = request.FILES.getlist("files")
+        path = request.data.get("path", "temp")
+
+        if not files:
+            return CustomResponse().successResponse(
+                {"error": "No file was provided."}, status=status.HTTP_400_BAD_REQUEST
+            )
+
+        uploaded_files = []
+
+        try:
+            for file_obj in files:
+                # Save each file to the default storage
+                sanitized_filename = add_unique_suffix_to_filename(sanitize_filename(file_obj.name))
+
+                file_path = default_storage.save(f"{path}/{sanitized_filename}", ContentFile(file_obj.read()))
+                file_url = settings.MEDIA_URL + file_path
+                uploaded_files.append(
+                    {"original_filename": file_obj.name, "file_url": file_url, "file_path": file_path}
+                )
+
+            return CustomResponse().successResponse(uploaded_files, status=status.HTTP_201_CREATED)
+
+        except Exception as e:
+            return CustomResponse().errorResponse(
+                {"error": str(e)}, description="File upload failed", status=status.HTTP_400_BAD_REQUEST
+            )
