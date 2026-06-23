bb784b77c14ea5992b65e75f695960eea606e52a
ranjith <padidalaranjith@gmail.com>
2026-06-23 16:44:29 +0530

    otp is configured.

--- shared/clients/sms.py
+++ shared/clients/sms.py
@@ -9,7 +9,6 @@ def send_otp_sms(mobile, otp):
             f"OTP is valid for 10 minutes. "
             f"Do not share this OTP with anyone."
         )
-
         url = (
             "https://full2ads.com/smsapi/index"
             f"?key=26911C63F0A654"
@@ -17,8 +16,8 @@ def send_otp_sms(mobile, otp):
             f"&routeid=1"
             f"&type=text"
             f"&contacts={mobile}"
-            f"&senderid=VOCABE"
-            f"&tlv=%7B%22DLT_ENTITY_ID%22%3A%221001548232379518414%22%2C%22DLT_TEMPLATE_ID%22%3A%221107178030754522073%22%7D"
+            f"&senderid=GENZPL"
+            f"&tlv=%7B%22DLT_ENTITY_ID%22%3A%221001548232379518414%22%2C%22DLT_TEMPLATE_ID%22%3A%221107178090064521662%22%7D"
             f"&msg={quote(message)}"
         )
         response = requests.get(
